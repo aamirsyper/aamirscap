6b33e865a81c42768d0b314dc6254530fe3eb798
Aamir Ansari <aamiransari@Aamirs-MacBook-Pro.local>
2023-04-11 10:29:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask.logging import create_logger
+import logging
+
+app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+LOG.info(f"Entered the Process ")
+
+@app.route('/')
+def home():
+    LOG.info(f"Entered the Home Function ")
+    html = f"<h3>Hello World, my name is Aamir Ansari</h3>"
+    return html.format(format)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
